d4743ebf129e824c21e905bb09e96313e2126b38
Aparna-tamil <pabarnatamil@gmail.com>
2020-07-26 08:04:03 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import SignUp from "./SignUp";
+import SignIn from './SignIn';
+import Root from './Root';
+import Card from './Card';
+
+
+function App() {
+    return (
+        <div>
+            
+            
+            <Root/>
+            <Card/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Root.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Route,Switch} from "react-router-dom";
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Home from './Home';
+import Card from './Card';
+
+//path
+
+
+const Root = () => {
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
+};
+
+export default Root;
